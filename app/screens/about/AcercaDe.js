fc1aeed31972223cb715c592f0d54b0075606f1b
Fabian <nfrfabian@gmail.com>
2021-11-28 18:30:29 -0300

    Se corrigio bug de falta de import

--- app/screens/about/AcercaDe.js
+++ app/screens/about/AcercaDe.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, ImageBackground} from 'react-native'
+import { StyleSheet, Text, View, ImageBackground, Linking} from 'react-native'
 
 export default function AcercaDe() {
     return (
@@ -12,16 +12,16 @@ export default function AcercaDe() {
                     It´s Sunny fue desarrollada en React Native utilizando Expo, en colaboración con los siguientes 
                     integrantes: {'\n'}{'\n'}
                         <Text style={styles.integrante}> 
-                            >> Nathalie Saravia: Splash Screen {'\n'}
+                            Nathalie Saravia. {'\n'}
                         </Text>
                         <Text style={styles.integrante}> 
-                            >> Franco Marelli: Agregar y quitar ciudades {'\n'}
+                            Franco Marelli. {'\n'}
                         </Text>
                         <Text style={styles.integrante}> 
-                            >> Nestor Robles: Listar y mostrar clima ciudades {'\n'}
+                            Nestor Robles. {'\n'}
                         </Text>
                         <Text style={styles.integrante}> 
-                            >> Martin Roige: Home y About {'\n'}{'\n'}
+                            Martin Roige. {'\n'}{'\n'}
                         </Text>
                         Desarrollamos la aplicación utilizando
                         <Text onPress={() => Linking.openURL('http://google.com')}>
